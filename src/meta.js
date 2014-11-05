bf18d93ece55ae9a17538eaf68136ce21c955ca4
zhangpeng <zhangpeng@baixing.com>
2014-11-05 23:13:09 +0800

    Update.

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name            Weibo-Keep-V5
-// @version         0.1.0
+// @version         0.1.2
 // @namespace       https://github.com/UserScript
+// @homepage        https://github.com/UserScript/Weibo-Keep-V5
 // @description     To keep Sina Weibo at V5.
 // @description:zh  让新浪微博保持在 V5 界面。
 // @downloadURL     https://rawgit.com/UserScript/Weibo-Keep-V5/master/dist/weibo-keep-v5.user.js
